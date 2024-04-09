a17c8a690f33d18faa1cf8513d18fdd638b85225
JavohirDev10 <javohirjonx@gmail.com>
2024-04-09 08:12:03 +0500

    Add Program File

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lv_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Bu bir qatorli izoh
+            Console.WriteLine("Men Rajabov JAvohir!");
+            Console.WriteLine("Ikkiming ikkinchi yilman!");
+            Console.Write("Samarqandlik man!");
+            Console.Write("    Tdtuda o`qiyman!");
+            Console.Write("   ikkinchi kursman!");
+             Console.Write("   elektironika fKda!");
+             
+
+
+            
+            
+        }
+    }
+}
